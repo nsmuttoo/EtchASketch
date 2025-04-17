f061cd36c7029a70ce25b30476007f834d47f1e3
nsmuttoo <nsmuttoo@gmail.com>
2025-04-17 19:02:19 -0400

    Set up start of grid

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Work")
+
+
+const container = document.querySelector("#container")
+var divList = []
+
+
+function fillDivList(height,width){
+size = height*width
+for(i =0; i<size; i++){
+divList[i] = document.createElement("div") 
+divList[i].classList.add("pixel")
+divList[i].innerHTML ="_"
+container.appendChild(divList[i])
+console.log(divList[i])
+}
+
+}
+
+fillDivList(16,16)
