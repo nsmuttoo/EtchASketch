ec802c8512d656afc4da52a301cff8814eb66804
nsmuttoo <nsmuttoo@gmail.com>
2025-04-20 23:38:50 -0400

    Completed project

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ var innerDivList = []
 var outerDivList =[]
 var store = []
 
+var topButton = document.createElement("Button")
+topButton.textContent = "Set Up Grid"
+document.body.prepend(topButton)
+
+
+
 function fillDivList(size){
 
 for(j=0; j<size; j++) {
@@ -20,19 +26,42 @@ console.log(innerDivList[i])
 }
 container.appendChild(outerDivList[j])
 }
+listen()
 }
+function deleteAll(){
+    var pixels = document.getElementsByClassName("pixel")
 
-var pixels = document.getElementsByClassName("pixel")
+    for (var i =0; i<pixels.length; i++){
+        pixels[i].remove()
+    
+}
+}
 
 function changeColour(){
     this.style.backgroundColor = "white"
 }
 
+function whenClick(){
+    var store = prompt("Enter you grid size")
+    console.log("click")
+    if(store<=100 && store >0){
+        deleteAll()
+        fillDivList(store)
+    }
+    else{
+        window.alert("Please enter a valid grid size (1-100)")
+    }
+}
 
 
 
-fillDivList(100)
+
+topButton.addEventListener("click",whenClick,true)
+
+function listen(){
+    var pixels = document.getElementsByClassName("pixel")
 
 for (var i =0; i<pixels.length; i++){
     pixels[i].addEventListener("mouseover", changeColour, false)
+}
 }
